26d731ddcb4ad507301e74f90f82ce807416033f
https://gitlab.globoi.com/daniel.rodrigues <daniel.rodrigues@g.globo>
2025-05-19 23:28:11 -0300

    TCC fix: nome de colunas ajustado e novo enum criado

--- test.sql
+++ test.sql
@@ -1,12 +1,40 @@
-CREATE TYPE race_enum AS ENUM ('WHITE', 'BLACK', 'ASIAN', 'NATIVE AMERICAN', 'HISPANIC', 'OTHER');
+CREATE TYPE race_enum AS ENUM ('WHITE', 'BLACK', 'ASIAN', 'UNKNOWN', 'HISPANIC', 'OTHERS');
 CREATE TYPE type_enum AS ENUM ('POLICE', 'VICTIM', 'CRIMINAL');
 CREATE TYPE threat_enum AS ENUM ('ATTACK', 'OTHER', 'UNKNOWN');
-CREATE TYPE flee_enum AS ENUM ('CAR', 'FOOT', 'OTHER', 'NOT FLEEING');
+CREATE TYPE flee_enum AS ENUM ('VECHILE', 'FOOT', 'UNKNOWN', 'NOT FLEEING');
+CREATE TYPE gender_enum AS ENUM('MALE', 'FEMALE', 'UNKNOWN', 'OTHERS');
 
--- Tabelas de dimensão
+CREATE TABLE DLocal (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  state VARCHAR(255) NOT NULL,
+  city VARCHAR(255) NOT NULL,
+  latitude DECIMAL(9,6) NOT NULL,
+  longitude DECIMAL(9,6) NOT NULL,
+  avgBlack REAL NOT NULL,
+  avgWhite REAL NOT NULL,
+  avgHispanic REAL NOT NULL,
+  avgAsian REAL NOT NULL,
+  standardDeviationBlack REAL NOT NULL,
+  standardDeviationWhite REAL NOT NULL,
+  standardDeviationHispanic REAL NOT NULL,
+  standardDeviationAsian REAL NOT NULL
+);
+
+CREATE TABLE DPerson (
+  id UUID NOT NULL,
+  idGroupAge INTEGER NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  gender gender_enum NOT NULL,
+  race race_enum NOT NULL,
+  typePerson type_enum NOT NULL,
+  rangeInf INTEGER NOT NULL,
+  rangeSup INTEGER NOT NULL,
+  PRIMARY KEY (id, idGroupAge)
+);
+
 CREATE TABLE DCrime (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  name VARCHAR(100) NOT NULL
+  name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE DDrug (
@@ -24,72 +52,9 @@ CREATE TABLE DDept (
   name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE DLocal (
-  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  state VARCHAR(255) NOT NULL,
-  city VARCHAR(255) NOT NULL,
-  lat DECIMAL(9,6) NOT NULL,
-  "long" DECIMAL(9,6) NOT NULL,
-  avgBlack REAL NOT NULL,
-  avgWhite REAL NOT NULL,
-  avgHispanic REAL NOT NULL,
-  avgAsian REAL NOT NULL,
-  standardDeviationBlack REAL NOT NULL,
-  standardDeviationWhite REAL NOT NULL,
-  standardDeviationHispanic REAL NOT NULL,
-  standardDeviationAsian REAL NOT NULL
-);
-
-CREATE TABLE DPerson (
-  id UUID NOT NULL,
-  idGroupAge INTEGER NOT NULL,
-  name VARCHAR(100) NOT NULL,
-  sex VARCHAR(1) NOT NULL,
-  race race_enum NOT NULL,
-  typePerson type_enum NOT NULL,
-  rangeInf INTEGER NOT NULL,
-  rangeSup INTEGER NOT NULL,
-  PRIMARY KEY (id, idGroupAge)
-);
-
 CREATE TABLE DWeapon (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  name VARCHAR(100) NOT NULL
-);
-
--- Tabelas de fatos
-CREATE TABLE FArrest (
-  idCrime UUID NOT NULL,
-  idLocal UUID NOT NULL,
-  idPerson UUID NOT NULL,
-  idGroupAge INTEGER NOT NULL,
-  idWeapon UUID NOT NULL,
-  idDrug UUID NOT NULL,
-  day INTEGER NOT NULL,
-  month INTEGER NOT NULL,
-  year INTEGER NOT NULL,
-  PRIMARY KEY (idCrime, idLocal, idPerson, idGroupAge, idWeapon, idDrug, day, month, year),
-  FOREIGN KEY (idCrime) REFERENCES DCrime (id),
-  FOREIGN KEY (idLocal) REFERENCES DLocal (id),
-  FOREIGN KEY (idPerson, idGroupAge) REFERENCES DPerson (id, idGroupAge),
-  FOREIGN KEY (idWeapon) REFERENCES DWeapon (id),
-  FOREIGN KEY (idDrug) REFERENCES DDrug (id)
-);
-
-CREATE TABLE FCrime (
-  idCrime UUID NOT NULL,
-  idPerson UUID NOT NULL,
-  idGroupAge INTEGER NOT NULL,
-  idWeapon UUID NOT NULL,
-  idLocal UUID NOT NULL,
-  day INTEGER NOT NULL,
-  month INTEGER NOT NULL,
-  year INTEGER NOT NULL,
-  PRIMARY KEY (idCrime, idPerson, idGroupAge, idWeapon, idLocal, day, month, year),
-  FOREIGN KEY (idCrime) REFERENCES DCrime (id),
-  FOREIGN KEY (idLocal) REFERENCES DLocal (id),
-  FOREIGN KEY (idPerson, idGroupAge) REFERENCES DPerson (id, idGroupAge),
-  FOREIGN KEY (idWeapon) REFERENCES DWeapon (id)
+  name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE FDeathPolice (
@@ -108,27 +73,6 @@ CREATE TABLE FDeathPolice (
   FOREIGN KEY (idDeathCause) REFERENCES DDeathCause (id)
 );
 
-CREATE TABLE FFatalEncounters (
-  idPerson UUID NOT NULL,
-  idDeathCause UUID NOT NULL,
-  idWeapon UUID NOT NULL,
-  idLocal UUID NOT NULL,
-  idGroupAge INTEGER NOT NULL,
-  idDept UUID NOT NULL,
-  day INTEGER NOT NULL,
-  month INTEGER NOT NULL,
-  year INTEGER NOT NULL,
-  threatLevel threat_enum NOT NULL,
-  flee flee_enum NOT NULL,
-  bodyCamera BOOLEAN,
-  PRIMARY KEY (idPerson),
-  FOREIGN KEY (idPerson, idGroupAge) REFERENCES DPerson (id, idGroupAge),
-  FOREIGN KEY (idDept) REFERENCES DDept (id),
-  FOREIGN KEY (idLocal) REFERENCES DLocal (id),
-  FOREIGN KEY (idDeathCause) REFERENCES DDeathCause (id),
-  FOREIGN KEY (idWeapon) REFERENCES DWeapon (id)
-);
-
 CREATE TABLE FShootings (
   idPerson UUID NOT NULL,
   idWeapon UUID NOT NULL,
@@ -145,5 +89,61 @@ CREATE TABLE FShootings (
   FOREIGN KEY (idPerson, idGroupAge) REFERENCES DPerson (id, idGroupAge),
   FOREIGN KEY (idLocal) REFERENCES DLocal (id),
   FOREIGN KEY (idCauseDeath) REFERENCES DDeathCause (id),
+  FOREIGN KEY (idWeapon) REFERENCES DWeapon (id)
+);
+
+CREATE TABLE FFatalEncounters (
+  idPerson UUID NOT NULL,
+  idGroupAge INTEGER NOT NULL,
+  idDeathCause UUID NOT NULL,
+  idWeapon UUID NOT NULL,
+  idDept UUID NOT NULL,
+  idLocal UUID NOT NULL,
+  threatLevel threat_enum NOT NULL,
+  flee flee_enum NOT NULL,
+  bodyCamera BOOLEAN,
+  year INTEGER NOT NULL,
+  month INTEGER NOT NULL,
+  day INTEGER NOT NULL,
+  PRIMARY KEY (idPerson),
+  FOREIGN KEY (idPerson, idGroupAge) REFERENCES DPerson (id, idGroupAge),
+  FOREIGN KEY (idDept) REFERENCES DDept (id),
+  FOREIGN KEY (idLocal) REFERENCES DLocal (id),
+  FOREIGN KEY (idDeathCause) REFERENCES DDeathCause (id),
+  FOREIGN KEY (idWeapon) REFERENCES DWeapon (id)
+);
+
+CREATE TABLE FArrest (
+  idCrime UUID NOT NULL,
+  idLocal UUID NOT NULL,
+  idPerson UUID NOT NULL,
+  idGroupAge INTEGER NOT NULL,
+  idWeapon UUID NOT NULL,
+  idDrug UUID NOT NULL,
+  year INTEGER NOT NULL,
+  month INTEGER NOT NULL,
+  day INTEGER NOT NULL,
+  PRIMARY KEY (idCrime, idLocal, idPerson, idGroupAge, idWeapon, idDrug, day, month, year),
+  FOREIGN KEY (idCrime) REFERENCES DCrime (id),
+  FOREIGN KEY (idLocal) REFERENCES DLocal (id),
+  FOREIGN KEY (idPerson, idGroupAge) REFERENCES DPerson (id, idGroupAge),
+  FOREIGN KEY (idWeapon) REFERENCES DWeapon (id),
+  FOREIGN KEY (idDrug) REFERENCES DDrug (id)
+);
+
+
+CREATE TABLE FCrime (
+  idCrime UUID NOT NULL,
+  idPerson UUID NOT NULL,
+  idGroupAge INTEGER NOT NULL,
+  idWeapon UUID NOT NULL,
+  idLocal UUID NOT NULL,
+  year INTEGER NOT NULL,
+  month INTEGER NOT NULL,
+  day INTEGER NOT NULL,
+  PRIMARY KEY (idCrime, idPerson, idGroupAge, idWeapon, idLocal, day, month, year),
+  FOREIGN KEY (idCrime) REFERENCES DCrime (id),
+  FOREIGN KEY (idLocal) REFERENCES DLocal (id),
+  FOREIGN KEY (idPerson, idGroupAge) REFERENCES DPerson (id, idGroupAge),
   FOREIGN KEY (idWeapon) REFERENCES DWeapon (id)
 );
