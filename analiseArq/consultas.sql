4c554af4c17a228a2d5c5fc8433c35dc80181591
Ryan Serra <ryan16serrasilva@gmail.com>
2025-05-21 14:39:12 -0300

    Subindo consultas SQL para o nosso modelo multimensional

---
+++ analiseArq/consultas.sql
@@ -0,0 +1,21 @@
+-- QUAIS FORAM AS PESSOAS que estão dentro das 10 maiores causa de morte e estão armadas
+SELECT DP.name,DP.gender,DP.race,DP.idGroupAge, DW.name,DDC.description, count(DDC.description) as qtd_kill FROM FShootings AS FS
+JOIN DPerson DP on FS.idPerson = DP.id and FS.idGroupAge = DP.idGroupAge
+JOIN DWeapon DW on FS.idWeapon = DW.id
+JOIN DDeathCause DDC on FS.idCauseDeath = DDC.id
+WHERE DDC.description != 'UNKNOWN' AND DW.name != 'UNKNOWN WEAPON'
+GROUP BY DP.name, DP.gender, DP.race, DP.idGroupAge, DW.name, DDC.description
+ORDER BY qtd_kill DESC
+LIMIT 10;
+
+-- Quais são os locais com maior inumero de crimes e quais foram as armas utilizadas
+
+SELECT DP.name,DP.race,DP.gender ,DW.name as weapon_name, DL.state, count(DL.state) as qtd_state FROM FCrime AS FC
+join DPerson DP on DP.id = FC.idPerson and DP.idGroupAge = FC.idGroupAge
+JOIN DLocal DL on DL.id = FC.idLocal
+JOIN DWeapon DW on FC.idWeapon = DW.id
+where DW.name != 'UNKNOWN' and dl.state != 'UNKNOWN'
+GROUP BY DP.race, DP.gender, DW.name, DL.state, DP.name
+ORDER BY  qtd_state DESC
+LIMIT 10;
+
