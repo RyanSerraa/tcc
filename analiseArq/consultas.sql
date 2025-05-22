aee8adea5c81827936085461f7b75de84e73f77e
Ryan Serra <ryan16serrasilva@gmail.com>
2025-05-22 10:06:59 -0300

    Subindo mais algumas consultas sql

--- analiseArq/consultas.sql
+++ analiseArq/consultas.sql
@@ -19,3 +19,22 @@ GROUP BY DP.race, DP.gender, DW.name, DL.state, DP.name
 ORDER BY  qtd_state DESC
 LIMIT 10;
 
+
+SELECT DP.gender, DP.race, DP.idGroupAge, DL.state, DL.city, DC.name as crime_name, COUNT(*) AS prisons_qtd   FROM FArrest
+    JOIN DPerson DP on DP.id = FArrest.idPerson and DP.idGroupAge = FArrest.idGroupAge
+    JOIN DWeapon DW on FArrest.idWeapon = DW.id
+    JOIN DLocal DL on FArrest.idLocal = DL.id
+    JOIN DCrime DC on FArrest.idCrime = DC.id
+    GROUP BY DP.gender, DP.race, DP.idGroupAge,DL.state, DL.city, DC.name, DP.id
+    ORDER BY prisons_qtd desc
+    LIMIT 10;
+
+
+SELECT DP.name,DP.race,DP.gender ,DW.name as weapon_name, DL.state, count(DP.id) as qtd_persons FROM FCrime AS FC
+join DPerson DP on DP.id = FC.idPerson and DP.idGroupAge = FC.idGroupAge
+JOIN DLocal DL on DL.id = FC.idLocal
+JOIN DWeapon DW on FC.idWeapon = DW.id
+where DW.name != 'UNKNOWN' and dl.state != 'UNKNOWN'
+GROUP BY DP.race, DP.gender, DW.name, DL.state, DP.name
+ORDER BY  qtd_persons DESC
+LIMIT 10;
