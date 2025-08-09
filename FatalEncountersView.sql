24582a13ade62f9fe2663c6ba74212effc57dd01
Ryan Serra <ryan16serrasilva@gmail.com>
2025-08-09 14:06:22 -0300

    DEV-009 Criação do chat bot na págin web e atualização da view de fatal

--- FatalEncountersView.sql
+++ FatalEncountersView.sql
@@ -6,24 +6,22 @@ SELECT
   ) AS date_of_death,
 
   COALESCE(dc.shortname, 'UNKNOWN') AS cause_of_death,
-  COALESCE(w.shortname, 'UNKNOWN') AS weapon_used,
+  COALESCE(w.shortname, 'UNKNOWN') AS weapon_used, --victim weapon used
   d.name AS police_department,
 
-  f.threatLevel AS threat_level,
-  f.flee AS flee_status,
+  f.threatLevel AS threat_status, -- possibles values: 'ATTACK', 'OTHER', 'UNKNOWN'
+  f.flee AS flee_status, -- possibles values 'VECHILE', 'FOOT', 'UNKNOWN', 'NOT FLEEING'
   f.bodyCamera AS is_police_wearing_camera,
 
   -- Dados da vítima (pessoa)
-  p.name AS victim_name,
-  p.gender AS victim_gender,
-  p.race AS victim_race,
-  p.typePerson AS victim_type,
+  p.gender AS victim_gender, -- possibles values: 'MALE', 'FEMALE', 'UNKNOWN', 'OTHERS'
+  p.race AS victim_race, -- 'WHITE', 'BLACK', 'ASIAN', 'UNKNOWN', 'HISPANIC', 'OTHERS'
+  p.typePerson AS victim_type, -- 'POLICE', 'VICTIM', 'CRIMINAL'
   p.rangeInf || ' - ' || p.rangeSup AS victim_age_range,
-  p.idGroupAge AS victim_age_group_id,
 
-  l.state AS state,
-  l.city AS city,
-  l.lat AS latitude,
+  l.state AS state, -- state of usa
+  l.city AS city, -- citys of usa
+  l.lat AS latitude, 
   l.long AS longitude
 
 FROM FFatalEncounters f
