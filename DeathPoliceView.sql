971d6ad950e3ece303d9b1a532199c3bd693ff4c
Ryan Serra <ryan16serrasilva@gmail.com>
2025-08-24 10:31:28 -0300

    DEV-019 Subindo correção das views

--- DeathPoliceView.sql
+++ DeathPoliceView.sql
@@ -1,22 +1,19 @@
 CREATE OR REPLACE VIEW VMortePolicial AS
 SELECT
-  TO_DATE(
-    f.year || '-' || LPAD(f.month::text, 2, '0') || '-' || LPAD(f.day::text, 2, '0'),
-    'YYYY-MM-DD'
-  ) AS data_morte,
+  MAKE_DATE(f.ano, f.mes, f.dia) AS data_crime,
 
-  COALESCE(dc.shortname, 'DESCONHECIDA') AS causa_morte,
-  d.name AS departamento_policia,
+  COALESCE(c.nome_abreviado, 'DESCONHECIDO') AS causa_morte,
 
-  p.tipo_pessoa AS tipo_policia, -- valor possível: 'POLICIAL'
-  
-  l.state AS estado, -- estado dos EUA
-  l.city AS cidade,  -- cidade dos EUA
-  l.lat AS latitude,
-  l.long AS longitude
+   p.tipo_pessoa AS tipo_policia, -- possíveis valores: 'POLICIAL'
+  p.faixa_inf || ' - ' || p.faixa_sup AS faixa_etaria_criminoso,
 
-FROM FMortePolicial f
-JOIN DCausaMorte dc ON f.id_causa_morte = dc.id
-JOIN DDepartamento d ON f.id_departamento = d.id
-JOIN DPessoa p ON f.idPerson = p.id AND f.id_faixa_etaria = p.id_faixa_etaria
+  l.estado AS estado, -- estado dos EUA
+  l.cidade AS cidade,  -- cidade dos EUA
+  l.latitude AS latitude,
+  l.longitude AS longitude
+
+FROM fmortepolicial f
+JOIN dcausamorte c ON f.id_causa_morte = c.id
+JOIN ddepartamento d ON f.id_departamento = d.id
+JOIN DPessoa p ON f.id_pessoa = p.id AND f.id_faixa_etaria = p.id_faixa_etaria
 JOIN DLocalidade l ON f.id_localidade = l.id;
