cfc97ab72090932ad437033d56460b742e89a248
Ryan Serra <ryan16serrasilva@gmail.com>
2025-08-24 10:31:28 -0300

    DEV-019 Subindo correção das views

--- CrimeView.sql
+++ CrimeView.sql
@@ -1,12 +1,9 @@
 CREATE OR REPLACE VIEW VCrime AS
 SELECT
-  TO_DATE(
-    f.year || '-' || LPAD(f.month::text, 2, '0') || '-' || LPAD(f.day::text, 2, '0'),
-    'YYYY-MM-DD'
-  ) AS data_crime,
+  MAKE_DATE(f.ano, f.mes, f.dia) AS data_crime,
 
-  COALESCE(c.shortname, 'DESCONHECIDO') AS nome_crime,
-  COALESCE(w.shortname, 'DESCONHECIDA') AS arma_usada, -- arma usada no crime
+  COALESCE(c.nome_abreviado, 'DESCONHECIDO') AS nome_crime,
+  COALESCE(a.nome_abreviado, 'DESCONHECIDA') AS arma_usada, -- arma usada no crime
 
   -- Dados da pessoa criminosa
   p.genero AS sexo_criminoso, -- possíveis valores: 'MASCULINO', 'FEMININO', 'DESCONHECIDO', 'OUTROS'
@@ -14,13 +11,13 @@ SELECT
   p.tipo_pessoa AS tipo_criminoso, -- possíveis valores: 'CRIMINOSO'
   p.faixa_inf || ' - ' || p.faixa_sup AS faixa_etaria_criminoso,
 
-  l.state AS estado, -- estado dos EUA
-  l.city AS cidade,  -- cidade dos EUA
-  l.lat AS latitude,
-  l.long AS longitude
+  l.estado AS estado, -- estado dos EUA
+  l.cidade AS cidade,  -- cidade dos EUA
+  l.latitude AS latitude,
+  l.longitude AS longitude
 
 FROM FCrime f
-JOIN DCrime c ON f.idCrime = c.id
-JOIN DArma w ON f.id_arma = w.id
-JOIN DPessoa p ON f.idPerson = p.id AND f.id_faixa_etaria = p.id_faixa_etaria
+JOIN DCrime c ON f.id_crime = c.id
+JOIN DArma a ON f.id_arma = a.id
+JOIN DPessoa p ON f.id_pessoa = p.id AND f.id_faixa_etaria = p.id_faixa_etaria
 JOIN DLocalidade l ON f.id_localidade = l.id;
--- ShootignsView.sql
+++ ShootignsView.sql
@@ -1,30 +1,28 @@
 CREATE OR REPLACE VIEW VTiroteio AS
 SELECT
-  TO_DATE(
-    f.year || '-' || LPAD(f.month::text, 2, '0') || '-' || LPAD(f.day::text, 2, '0'),
-    'YYYY-MM-DD'
-  ) AS data_tiro,
+  MAKE_DATE(f.ano, f.mes, f.dia) AS data_crime,
 
-  COALESCE(dc.shortname, 'DESCONHECIDA') AS causa_morte,
-  COALESCE(w.shortname, 'DESCONHECIDA') AS arma_usada, -- arma usada pela vítima
+  COALESCE(c.nome_abreviado, 'DESCONHECIDO') AS causa_morte,
+  COALESCE(a.nome_abreviado, 'DESCONHECIDA') AS arma_usada, -- arma usada no crime
 
   f.status_de_ameaca AS status_ameaca, -- possíveis valores: 'ATAQUE', 'OUTROS', 'DESCONHECIDO'
   f.fuga AS status_fuga, -- possíveis valores: 'VEÍCULO', 'A PÉ', 'NÃO FUGIU', 'DESCONHECIDO'
   f.camera_corporal AS policial_com_camera,
 
-  -- Dados da vítima
+  -- Dados da pessoa presa
   p.genero AS sexo_vitima, -- possíveis valores: 'MASCULINO', 'FEMININO', 'DESCONHECIDO', 'OUTROS'
-  p.raca AS raca_vitima,   -- possíveis valores: 'BRANCO', 'NEGRO', 'ASIÁTICO', 'HISPÂNICO', 'DESCONHECIDO', 'OUTROS'
-  p.tipo_pessoa AS tipo_vitima, -- valores possíveis: 'POLICIAL', 'VÍTIMA', 'CRIMINOSO'
-  p.faixa_inf || ' - ' || p.faixa_sup AS faixa_etaria_vitima,
+  p.raca AS raca_vitima,   -- possíveis valores: 'BRANCO', 'NEGRO', 'ASIÁTICO', 'DESCONHECIDO', 'HISPÂNICO', 'OUTROS'
+  p.tipo_pessoa AS tipo_vitima, -- possíveis valores: 'VITIMA'
+  p.faixa_inf || ' - ' || p.faixa_sup AS faixa_etaria_criminoso,
 
-  l.state AS estado, -- estado dos EUA
-  l.city AS cidade,  -- cidade dos EUA
-  l.lat AS latitude,
-  l.long AS longitude
+  l.estado AS estado, -- estado dos EUA
+  l.cidade AS cidade,  -- cidade dos EUA
+  l.latitude AS latitude,
+  l.longitude AS longitude
 
 FROM FTiroteio f
-JOIN DCausaMorte dc ON f.id_causa_morte = dc.id
-JOIN DArma w ON f.id_arma = w.id
-JOIN DPessoa p ON f.idPerson = p.id AND f.id_faixa_etaria = p.id_faixa_etaria
+JOIN DArma a ON f.id_arma = a.id
+JOIN dcausamorte c ON f.id_causa_morte = c.id
+JOIN DPessoa p ON f.id_pessoa = p.id AND f.id_faixa_etaria = p.id_faixa_etaria
 JOIN DLocalidade l ON f.id_localidade = l.id;
+
