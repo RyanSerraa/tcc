d63fbdb3699901071184805334fe14dded907913
Daniel Rodrigues <daniyyelrodrigues@gmail.com>
2025-09-20 15:15:07 -0300

    feat: integrate Gerente, Analista, and Redator agents into AgentManager; update Main to load new agents

--- src/application/agent_orchestrator.py
+++ src/application/agent_orchestrator.py
@@ -1,7 +1,6 @@
 # agent_manager.py
 from langgraph.graph import END, START, StateGraph
 
-from src.application.run_query import RunQuery
 from src.domain.state import State
 
 
@@ -14,17 +13,24 @@ class AgentManager:
         text_editor_agent,
         chart_editor_agent,
         web_search_agent,
+        run_query_agent,
         supervisor_agent,
+        gerente_agent,
         embeddings,
+        analista_agent,
+        redator_agent,
     ):
         self.connection = connection
         self.text_to_sql = text_to_sql_agent
         self.text_editor = text_editor_agent
         self.chart_editor = chart_editor_agent
+        self.gerente = gerente_agent
         self.web_search = web_search_agent
         self.embeddings = embeddings
-        self.run_query = RunQuery(connection).run_query
+        self.run_query = run_query_agent
         self.supervisor = supervisor_agent
+        self.analista = analista_agent
+        self.redator = redator_agent
         self.workflow = StateGraph(State)
         self._build_workflow()
         self.chain = self.workflow.compile()
@@ -43,32 +49,70 @@ class AgentManager:
                 state, self.embeddings, self.connection
             ),
         )
-        self.workflow.add_node("run_query", self.run_query)
         self.workflow.add_node(
-            "respondWithChart", lambda state: self.chart_editor.respond(state)
+            "gerente", lambda state: self.gerente.choose_chain(state)
         )
         self.workflow.add_node(
-            "respondWithText", lambda state: self.text_editor.respond(state)
+            "run_query", lambda state: self.run_query.run_query(state)
         )
+        self.workflow.add_node(
+            "chartEditor", lambda state: self.chart_editor.respond(state)
+        )
+        self.workflow.add_node(
+            "textEditor", lambda state: self.text_editor.respond(state)
+        )
+        self.workflow.add_node("analista", lambda state: self.analista.respond(state))
+        self.workflow.add_node("redator", lambda state: self.redator.respond(state))
 
         self.workflow.add_edge(START, "supervisor")
         self.workflow.add_conditional_edges(
             "supervisor",
-            self.verifySupervisorAnswer,
-            {"Yes": "to_sql_query", "No": "searchWeb"},
+            self.verifySupervisorResponse,
+            {"Sim": "to_sql_query", "Não": "searchWeb"},
         )
         self.workflow.add_edge("to_sql_query", "run_query")
+        self.workflow.add_edge("run_query", "gerente")
         self.workflow.add_conditional_edges(
-            "run_query",
-            self.hasChart,
-            {"Yes": "respondWithChart", "No": "respondWithText"},
+            "gerente",
+            self.verifyGerenteResponse,
+            {
+                "textEditor": "textEditor",
+                "chartEditor": "chartEditor",
+                "analista": "analista",
+            },
         )
-        self.workflow.add_edge("respondWithText", END)
-        self.workflow.add_edge("respondWithChart", END)
+
+        self.workflow.add_edge("textEditor", "redator")
+        self.workflow.add_edge("chartEditor", "redator")
+        self.workflow.add_edge("analista", "redator")
+        # self.workflow.add_edge("redator", END)
+        self.workflow.add_conditional_edges(
+            "redator",
+            self.verifyRedatorResponse,
+            {"refazerGrafico": "chartEditor", None: END},
+        )
         self.workflow.add_edge("searchWeb", END)
 
-    def verifySupervisorAnswer(self, state: State):
-        return "Yes" if state.get("isEUA") else "No"
+    def verifySupervisorResponse(self, state: State):
+        return "Sim" if state.isEUA else "Não"
 
-    def hasChart(self, state: State):
-        return "Yes" if "gráfico" in state.get("question", "").lower() else "No"
+    def verifyGerenteResponse(self, state: State):
+        decision = state.gerente_decision
+        outputs = []
+
+        if decision.get("textEditor") == "sim":
+            outputs.append("textEditor")
+        if decision.get("chartEditor") == "sim":
+            outputs.append("chartEditor")
+        if decision.get("analista") == "sim":
+            outputs.append("analista")
+
+        return outputs
+
+    def verifyRedatorResponse(self, state: State):
+        # print(f"Verificando resposta do Redator: {state.get('redator_response')}")
+        decision = state.redator_response
+        if decision.get("redoChart") == True:
+            return "refazerGrafico"
+
+        return None
--- src/main.py
+++ src/main.py
@@ -9,6 +9,10 @@ from src.application.query_manager import QueryManager
 from src.application.supervisor import Supervisor
 from src.application.text_editor import TextEditor
 from src.application.text_to_sql import TextToSQL
+from src.application.gerente import Gerente
+from src.application.analista import Analista
+from src.application.redator import Redator
+from src.application.run_query import RunQuery
 from src.application.web_search import WebSearch
 from src.infrastructure.ai_agents import Agents
 from src.infrastructure.db import db
@@ -38,6 +42,10 @@ class Main:
             )
             web_search_agent = WebSearch(Agents.load_agent(*self.config.web_search))
             supervisor_agent = Supervisor(Agents.load_agent(*self.config.supervisor))
+            gerente_agent = Gerente(Agents.load_agent(*self.config.gerente))
+            analista_agent = Analista(Agents.load_agent(*self.config.analista))
+            redator_agent = Redator(Agents.load_agent(*self.config.redator))
+
             logging.info("Todos os agentes carregados com sucesso")
         except Exception as e:
             logging.error(f"Erro ao carregar agentes: {e}", exc_info=True)
@@ -59,6 +67,7 @@ class Main:
                     "Database URL (db_url) is not set in the configuration."
                 )
             connection = db().get_connection(self.config.db_url)
+            run_query_agent = RunQuery(connection)
             agent_manager = AgentManager(
                 connection=connection,
                 text_to_sql_agent=text_to_sql_agent,
@@ -67,6 +76,10 @@ class Main:
                 web_search_agent=web_search_agent,
                 supervisor_agent=supervisor_agent,
                 embeddings=embeddings,
+                gerente_agent=gerente_agent,
+                run_query_agent=run_query_agent,
+                analista_agent=analista_agent,
+                redator_agent=redator_agent,
             )
 
             query_manager = QueryManager(agent_manager=agent_manager)
