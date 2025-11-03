09f7cac729ee416bdb86106833a68f06a28ad499
Daniel Rodrigues <daniyyelrodrigues@gmail.com>
2025-11-03 12:28:06 -0300

    refactor: replace HuggingFaceEmbeddings with SentenceTransformer for improved embedding model initialization

--- src/infrastructure/embeddings.py
+++ src/infrastructure/embeddings.py
@@ -1,5 +1,4 @@
-import torch
-from langchain_huggingface import HuggingFaceEmbeddings
+from sentence_transformers import SentenceTransformer
 from psycopg2.extras import Json
 
 from src.infrastructure.db import DB
@@ -7,23 +6,17 @@ from src.infrastructure.db import DB
 
 class Embeddings:
     def __init__(self):
-        if torch.cuda.is_available():
-            self.embeddings_model = HuggingFaceEmbeddings(
-                model_name="sentence-transformers/all-MiniLM-L6-v2",
-                model_kwargs={"device": "cuda", "trust_remote_code": True},
-            )
-        else:
-            self.embeddings_model = HuggingFaceEmbeddings(
-                model_name="sentence-transformers/all-MiniLM-L6-v2",
-                model_kwargs={"device": "cpu", "trust_remote_code": True},
-            )
+        self.embeddings_model = SentenceTransformer(
+            "sentence-transformers/all-MiniLM-L6-v2", device="cpu"
+        )
 
     def embed_query(self, question: str):
-        return self.embeddings_model.embed_query(question)
+        return self.embeddings_model.encode(question)
 
     def getContext(self, question: str, agente: str, db: DB) -> str:
         query_emb = self.embed_query(question)
-        query_emb_vector = Json(query_emb)
+        query_emb_list = query_emb.tolist()
+        query_emb_vector = Json(query_emb_list)
         resultados = []
 
         resultados = db.execute_query(
